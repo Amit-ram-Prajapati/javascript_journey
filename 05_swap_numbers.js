a6c35cb6b3d1df1ff665dbdbec7e4d3717fbeb78
Amit Prajapati <243986054+Amit-ram-Prajapati@users.noreply.github.com>
2026-01-05 17:26:16 +0530

    swapping of two numbers using third variable

---
+++ 05_swap_numbers.js
@@ -0,0 +1,7 @@
+let num = 20 , num1 = 30;
+let temp = 0;
+console.log(`Before swapping numbers are ${num} ${num1}` )
+temp = num ; // 20
+num = num1; // 30
+num1 = temp ; // 20
+console.log(`After swapping numbers are ${num} ${num1}` )
